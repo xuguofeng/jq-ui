41360a566d50e3fca8fbdcc564b1f2620f5fff6a
xuguofeng <xuguofeng1990@126.com>
2018-04-03 14:04:08 +0800

    dialog插件的js函数、css样式、示例基本完成

--- js/dialog.js
+++ js/dialog.js
@@ -1,77 +1,90 @@
-(function($){
+(function($) {
+	
+	// dialog的z-index样式，随着dialog的增加而增加
+	var zIndex = 1100;
 
-	$.extend({dialog:{}});
-
-	// dialog信息缓存
-	var dialogCache = {};
-
-	/*
-		生成模态窗口的遮挡层
-	*/
-	function createMask(){
-		var $mask = $("<div class=\"dialog-mask\"></div>");
-		return $mask;
+	/**
+	 * 初始化dialog的html结构
+	 * 
+	 * @param {Object} id
+	 */
+	function initDialog(id, title) {
+		// 对话框DIV
+		var $dialog = $("<div></div>")
+				.attr("id", id)
+				.addClass("dialog-container");
+		// header
+		var $header = $("<div></div>")
+				.addClass("dialog-header")
+				.append($("<div>" + title + "</div>").addClass("dialog-title"))
+				.append($("<div></div>").addClass("dialog-close"));
+		// content
+		var $content = $("<div></div>")
+				.addClass("dialog-content");
+		return $dialog.append($header).append($content);
 	}
 
-	/*
-		初始化一个dialog的页面DOM结构
-	*/
-	function initDialog(id){
-		var $dialog = $("<div id=\""+id+"\" class=\"dialog\"></div>");
-		$dialog.append("<div class=\"dialog-title\"><span></span><div class=\"dialog-close\"><a href=\"javascript:void(0)\"></a></div></div>");
-		$dialog.append("<div class=\"dialog-content\"></div>");
-		return $dialog;
-	}
-
-	/*
-		为dialog结构设置css样式
-	*/
-	function initDialogStyle(dialogEle,settings){
+	/**
+	 * 初始化dialog各个组成部分的大小
+	 * 
+	 * @param {Object} dialog
+	 * @param {Object} options
+	 */
+	function initSize(dialog, options) {
+		// 获取屏幕的宽高
 		var screenWidth = $(document.body).width();
 		var screenHeight = $(window).height();
-		dialogEle.css({"width":settings.width,"height":settings.height,"left":(screenWidth-settings.width)/2,"top":(screenHeight-settings.height)/2});
-		dialogEle.css("z-index",101);
-		dialogEle.find("div.dialog-title span").html(settings.title);
-		dialogEle.find("div.dialog-title").css({"width":settings.width-20});
-		dialogEle.find("div.dialog-content").css({"width":settings.width-20,"height":settings.height-35});
+		// 设置对话框div大小、位置
+		dialog.css({
+			"width": options["width"],
+			"height": options["height"],
+			"top": (screenHeight - options["height"])/3,
+			"left": (screenWidth - options["width"])/2
+		});
+		// 设置内容div大小
+		dialog.find("div.dialog-content")
+			.css({
+				"width": options["width"] - 32,
+				"height": options["height"] - 57
+			});
 	}
 
-	/*
-		添加关闭事件
-	*/
-	function bindEvent(dialogElement){
-		dialogElement.find("div.dialog-title .dialog-close").click(function(){
-			$(this).parent().parent().hide().remove();
-
-			var id = $(this).parent().parent().attr("id");
-			dialogCache[id] = null;
-
+	/**
+	 * 初始化关闭事件
+	 * 
+	 * @param {Object} dialog
+	 */
+	function bindEvent(dialog) {
+		dialog.find("div.dialog-close").click(function(){
+			dialog.remove();
 			// 如果是模态窗口，还需要把mask层关掉
-			if ($(".dialog-mask")[0]) {
+			if ($(".dialog-mask")[0])
 				$(".dialog-mask").remove();
-			}
 		});
 	}
 
-	/*
-		添加拖拽事件
-	*/
-	function bindDragEvent(dialogElement){
+	/**
+	 * 初始化拖动事件
+	 * 
+	 * @param {Object} dialog
+	 */
+	function bindDrag(dialog) {
 		var mX=0,mY=0; // 定义鼠标X轴Y轴
 		var dX=0,dY=0; // 定义div左、上位置
 		var isDown = false; // mousedown标记
 
 		if(document.attachEvent) {
-			dialogElement[0].attachEvent('onselectstart', function() {
+			dialog[0].attachEvent('onselectstart', function() {
 				return false;
 			});
 		}
-		dialogElement.find("div.dialog-title").mousedown(function(event) {
+		dialog.find("div.dialog-header").mousedown(function(event) {
+			this.style.cursor = "move";
 			var event = event || window.event;
 			mX = event.screenX;
 			mY = event.screenY;
-			dX = parseFloat(dialogElement.css("left"));
-			dY = parseFloat(dialogElement.css("top"));
+			dX = parseFloat(dialog.css("left"));
+			dY = parseFloat(dialog.css("top"));
 			isDown = true;
 		});
 		$(document).mousemove(function(event) {
@@ -79,125 +92,133 @@
 			var x = event.screenX;
 			var y = event.screenY;
 			if(isDown) {
-				dialogElement.css({"left": x - mX + dX, "top": y - mY + dY});//div动态位置赋值
+				dialog.css({"left": x - mX + dX, "top": y - mY + dY}); // div动态位置赋值
 			}
 		});
 
 		$(document).mouseup(function() {
-			isDown = false;//鼠标拖拽结束
+			dialog.find("div.dialog-header").css("cursor", "default");
+			isDown = false; // 鼠标拖拽结束
 		});
 	}
 
-	/*
-		加载dialog内容。
-		判断dialog的加载方式，
-		如果是remote，调用loadFromRemoteUrl方法
-		如果是jquery元素，直接向窗口内容中追加即可
-	*/
-	function load(settings,dialogEle){
-		var type = settings.loadType;
-		switch (type) {
-			case 'remote':
-				loadFromRemoteUrl(settings.loadData,dialogEle);
-				break;
-			case 'jquery':
-				dialogEle.find("div.dialog-content").html(settings.loadData);
-				break;
-			default:
-				dialogEle.find("div.dialog-content").html("");
-				break;
+	/**
+	 * 加载内容
+	 * 
+	 * @param {Object} dialog
+	 * @param {Object} options
+	 */
+	function load(dialog, options) {
+		// 优先使用content配置填充内容
+		var content = options["content"];
+		if(content) {
+			dialog.find("div.dialog-content").html(content);
+			return;
 		}
+		// 使用url进行远程获取
+		var url = options["url"];
+		if(url)
+			ajaxLoad(dialog, url);
 	}
-
-	/*
-		从远程url加载html内容，被load函数调用
-	*/
-	function loadFromRemoteUrl(url,dialogEle){
+	
+	/**
+	 * 使用ajax从远程url获取dialog内容
+	 * 
+	 * @param {Object} dialog
+	 * @param {Object} url 
+	 */
+	function ajaxLoad(dialog, url) {
 		$.ajax({
-			dataType:"html",
-			url:url+"?d="+new Date().getTime(),
-			cache:false,
-			success:function(data){
-				dialogEle.find("div.dialog-content").html(data);
+			type: "post",
+			dataType: "html",
+			url: url,
+			cache: false,
+			success: function(data) {
+				// 填充数据到面板
+				dialog.find("div.dialog-content").html(data);
 			}
 		});
 	}
 
-	/*
-		刷新dialog
-	*/
-	function reload(dialogId){
-		// 如果dialog不存在，直接返回false
-		if (!dialogCache[dialogId]) return false;
-		var ele = $("#"+dialogId);
-		load(dialogCache[dialogId],ele);
+	/**
+	 * 创建body遮罩层
+	 */
+	function createMask() {
+		return $("<div class='dialog-mask'></div>").css("z-index", ++zIndex).appendTo($("body"));
 	}
 
-	/*
-		关闭dialog
-	*/
-	function close(dialogId){
-		// 如果dialog不存在，直接返回false
-		if (!dialogCache[dialogId]) return false;
-		var ele = $("#"+dialogId);
-		ele.hide().remove();
-	}
-
-	$.extend($.dialog,{
-		open : function(settings){
-
-			// 默认参数项
+	$.dialog = {
+		// 打开一个dialog
+		open: function(options) {
+			// 默认配置
 			var defaultSettings = {
-				title       :  "对话框", // 标题
-				id          :  undefined,// 生成dialog的id
-				loadType    :  "remote", // 加载方式，remote|jquery
-				loadData    :  undefined,// 数据，会根据加载方式参数的不同进行不同的操作
-				width       :  500,      // 宽
-				height      :  300,      // 高
-				modal       :  false,    // 模态
-				dragable    :  true      // 拖拽
+				id: undefined,
+				title: undefined,
+				width: 500,
+				height: 300,
+				modal: false,
+				url: '',
+				content: ''
 			};
-
 			// 合并默认配置和用户配置参数
-			settings = $.extend(defaultSettings, settings);
+			var options = $.extend(defaultSettings, options);
 
-			var dialogId = settings.id;
-			
-			if(dialogId===undefined||
-				dialogId==null||
-				dialogId.replace(/(^\s*)|(\s*$)/g,"")==""){
-				dialogId = "dialog-"+new Date().getTime();
+			// 获取对话框div的id
+			var dialogId = options[ "id" ];
+
+			if(dialogId === undefined || dialogId == null || dialogId.replace(/(^\s*)|(\s*$)/g,"") == "") {
+				dialogId = "jquery_dialog_"+new Date().getTime();
 			}
 
-			// 如果dialog存在，直接返回false
-			if (dialogCache[dialogId]) return false;
+			// 查看是否已经存在这个id的dialog
+			if($("#" + dialogId)[0])
+				return $("#" + dialogId);
 
-			// 将dialog配置信息缓存在dialogCache对象中
-			dialogCache[dialogId] = settings;
+			// 对话框title
+			var title = options[ "title" ];
+			if(title == undefined || title == null)
+				title = dialogId;
 
-			var dialogEle = initDialog(dialogId);
-
-			initDialogStyle(dialogEle,settings);
-			bindEvent(dialogEle);
-			bindDragEvent(dialogEle);
-
-			load(settings,dialogEle);
+			// 创建对话框
+			var dialog = initDialog(dialogId, title);
+			initSize(dialog, options);
+			bindEvent(dialog);
+			bindDrag(dialog);
+			load(dialog, options);
 
 			// 如果是模态窗口，需要创建遮挡层
-			if (settings.modal) {
-				var mask = createMask();
-				mask.appendTo($("body"));
-			}
+			if (options[ 'modal' ])
+				createMask();
 
-			dialogEle.css("display","block").appendTo($("body"));
-			
+			// 把对话框追加到body末尾
+			dialog.css({"display": "block", "z-index": ++zIndex}).appendTo($("body"));
+
+			// 缓存
+			$.data(dialog[0], "dialog", options);
+
+			// 返回这个dialog
+			return dialog;
 		},
-		reload : function(id){
-			reload(id);
+		// 根据id关闭指定dialog
+		close: function(id) {
+			var dialog = $("#" + id);
+			if(dialog[0]) {
+				dialog.remove();
+				// 如果是模态窗口，还需要把mask层关掉
+				if ($(".dialog-mask")[0])
+					$(".dialog-mask").remove();
+			}
 		},
-		close : function(id){
-			close(id);
+		// 根据id刷新指定dialog
+		reload: function(id) {
+			var dialog = $("#" + id);
+			if(dialog[0]) {
+				var options = $.data(dialog[0], "dialog");
+				if(options["url"]) {
+					ajaxLoad(dialog, options["url"]);
+				}
+			}
 		}
-	});
+	};
 
 })(jQuery);
