45a933837934134dca8a74561e597e7ed7bc2273
xuguofeng <xuguofeng1990@126.com>
2018-05-03 14:31:37 +0800

    优化函数名

--- js/tree.js
+++ js/tree.js
@@ -4,7 +4,7 @@
 	 * 初始化tree的DOM结构
 	 * @param {jQuery} $tree
 	 */
-	function initTree($tree) {
+	function _initTree($tree) {
 		$tree.addClass("st_tree")
 			.find("li[class!='node']").addClass("node").end()
 			.find("li:has('ul')").removeClass("node").addClass("folder");
@@ -18,14 +18,14 @@
 	 * @param {jQuery} $tree
 	 * @param {Object} options
 	 */
-	function bindEvent($tree, options) {
+	function _bindEvent($tree, options) {
 		$tree.find("li.folder > a").on("click", function(e) {
 			var f = $(this).parent();
 			if(f.hasClass("open")) {
 				f.removeClass("open").children("ul").css("display", "none");
 			} else {
 				if(f.children("ul").length == 0) {
-					loadNodes(f, options);
+					_loadNodes(f, options);
 				}
 				f.addClass("open").children("ul").css("display", "block");
 			}
@@ -53,7 +53,7 @@
 	 *		}
 	 *	]
 	 */
-	function createNodes($target, data, options) {
+	function _createNodes($target, data, options) {
 		if(!data || data.length == 0) { return ""; }
 		var $ul = $("<ul></ul>");
 		for(var i = 0; i < data.length; i++) {
@@ -77,11 +77,11 @@
 			$node.append($a);
 
 			if(d["type"] || (d["children"] && d["children"].length > 0)) {
-				var $nodes = createNodes($target, d["children"], options);
+				var $nodes = _createNodes($target, d["children"], options);
 				$node.append($nodes).removeClass("node").addClass("folder");
 				if(d["open"]) {
 					$node.attr("open", "");
-					loadNodes($node, options);
+					_loadNodes($node, options);
 				}
 			}
 			$ul.append($node);
@@ -94,7 +94,7 @@
 	 * @param {jQuery} $target
 	 * @param {Object} options
 	 */
-	function loadNodes($target, options) {
+	function _loadNodes($target, options) {
 		if(!options["url"]) { return; }
 		var id = 0;
 		if($target && $target.attr("node-id")) {
@@ -106,9 +106,9 @@
 			url: options["url"] + "?id=" + id,
 			cache: false,
 			success: function(data) {
-				$target.append(createNodes($target, data, options));
-				initTree($target);
-				bindEvent($target, options);
+				$target.append(_createNodes($target, data, options));
+				_initTree($target);
+				_bindEvent($target, options);
 			}
 		});
 	}
@@ -168,17 +168,17 @@
 			var tree = $(this);
 
 			// 从json数组创建tree节点
-			var nodes = createNodes(tree, options["data"], options);
+			var nodes = _createNodes(tree, options["data"], options);
 			tree.append(nodes);
 
 			// 从url加载数据
-			loadNodes(tree, options);
+			_loadNodes(tree, options);
 
 			// 初始化tree结构
-			initTree(tree);
+			_initTree(tree);
 
 			// 给folder绑定事件
-			bindEvent(tree, options);
+			_bindEvent(tree, options);
 		});
 	};
 })(jQuery);
