89893b074e4ee0fd1e4b966333f60e2a5c9cf763
xuguofeng <xuguofeng1990@126.com>
2018-05-03 14:23:41 +0800

    完成tree插件节点点击事件响应

--- js/tree.js
+++ js/tree.js
@@ -2,7 +2,7 @@
 
 	/**
 	 * 初始化tree的DOM结构
-	 * @param {Object} $tree
+	 * @param {jQuery} $tree
 	 */
 	function initTree($tree) {
 		$tree.addClass("st_tree")
@@ -15,17 +15,17 @@
 
 	/**
 	 * 给folder绑定事件
-	 * @param {Object} $tree
-	 * @param {Object} url
+	 * @param {jQuery} $tree
+	 * @param {Object} options
 	 */
-	function bindEvent($tree, url) {
+	function bindEvent($tree, options) {
 		$tree.find("li.folder > a").on("click", function(e) {
 			var f = $(this).parent();
 			if(f.hasClass("open")) {
 				f.removeClass("open").children("ul").css("display", "none");
 			} else {
 				if(f.children("ul").length == 0) {
-					loadNodes(f, url);
+					loadNodes(f, options);
 				}
 				f.addClass("open").children("ul").css("display", "block");
 			}
@@ -34,46 +34,54 @@
 	
 	/**
 	 * 从json数组创建tree节点
+	 * @param {jQuery} $target
 	 * @param {Object} data
+	 * @param {Object} options
 	 *	[
 	 *		{
 	 *			id: 1, 
-	 *			text: "",
-	 *			open: true|false,
-	 *			type: "folder",
-	 *			children: [],
+	 *			text: "", 
+	 *			open: true|false, 
+	 *			type: "folder", 
+	 *			children: [], 
 	 *			url: ""
 	 *		}, 
 	 *		{
 	 *			id: 2, 
-	 *			text: "",
+	 *			text: "", 
 	 *			url: ""
-	 *		}, 
-	 *		......
+	 *		}
 	 *	]
 	 */
-	function createNodes(data, url) {
-		if(!data || data.length == 0) {
-			return "";
-		}
+	function createNodes($target, data, options) {
+		if(!data || data.length == 0) { return ""; }
 		var $ul = $("<ul></ul>");
 		for(var i = 0; i < data.length; i++) {
 			var d = data[i];
-			var $node = $("<li></li>")
-					.addClass("node")
-					.attr("node-id", "tree-node-" + d["id"])
-					.append($("<a></a>").attr("href", d["url"] ? d["url"] : "#").html(d["text"]).prepend("<span><span>"));
+			var $node = $("<li></li>").addClass("node")
+										.attr("node-id", "tree-node-" + d["id"]);
 
-			// 判断d.url是不是undefined|null|""|#
-			// 判断d.onClick是否存在
-			// 以上两点都正确，才在这里给a标签设置点击方法
+			var $a = $("<a></a>").html(d["text"]).prepend("<span><span>");
+			if(d["url"]) {
+				$a.attr("href", d["url"]);
+			} else if(options["onClick"] && !d["type"]) {
+				// 注意这个闭包
+				$a.attr("href", "javascript:void(0);").click((function(d) {
+					return function() {
+						options["onClick"](d);
+					}
+				})(d));
+			} else {
+				$a.attr("href", "javascript:void(0);");
+			}
+			$node.append($a);
 
 			if(d["type"] || (d["children"] && d["children"].length > 0)) {
-				var $nodes = createNodes(d["children"], url);
+				var $nodes = createNodes($target, d["children"], options);
 				$node.append($nodes).removeClass("node").addClass("folder");
 				if(d["open"]) {
 					$node.attr("open", "");
-					loadNodes($node, url);
+					loadNodes($node, options);
 				}
 			}
 			$ul.append($node);
@@ -83,13 +91,11 @@
 
 	/**
 	 * 从url加载数据
-	 * @param {Object} $target
-	 * @param {Object} url
+	 * @param {jQuery} $target
+	 * @param {Object} options
 	 */
-	function loadNodes($target, url) {
-		if(!url) {
-			return;
-		}
+	function loadNodes($target, options) {
+		if(!options["url"]) { return; }
 		var id = 0;
 		if($target && $target.attr("node-id")) {
 			id = $target.attr("node-id").replace("tree-node-", "");
@@ -97,14 +103,32 @@
 		$.ajax({
 			type: "get",
 			dataType: "json",
-			url: url + "?id=" + id,
+			url: options["url"] + "?id=" + id,
 			cache: false,
 			success: function(data) {
-				$target.append(createNodes(data, url));
+				$target.append(createNodes($target, data, options));
 				initTree($target);
-				bindEvent($target, url);
+				bindEvent($target, options);
 			}
 		});
+	}
+
+	/**
+	 * 打开指定节点，前提是该节点已经存在且是一个folder
+	 * @param {jQuery} $tree
+	 * @param {int} folderNodeId
+	 */
+	function open($tree, folderNodeId) {
+		// TODO
+	}
+
+	/**
+	 * 关闭指定节点，前提是该节点已经存在且是一个folder
+	 * @param {jQuery} $tree
+	 * @param {int} folderNodeId
+	 */
+	function close($tree, folderNodeId) {
+		// TODO
 	}
 
 	$.fn.tree = function(options, param) {
@@ -124,13 +148,11 @@
 					});
 				case 'open':
 					return this.each(function() {
-						// TODO
-						// open($(this), param);
+						open($(this), param);
 					});
 				case 'close':
 					return this.each(function() {
-						// TODO
-						// close($(this), param);
+						close($(this), param);
 					});
 			}
 		}
@@ -138,21 +160,25 @@
 		options = options || {};
 
 		return this.each(function() {
+
+			// 缓存配置选项
+			$.data(this, "tree", options);
+
 			// 保存对象
 			var tree = $(this);
 
 			// 从json数组创建tree节点
-			var nodes = createNodes(options["data"], options["url"]);
+			var nodes = createNodes(tree, options["data"], options);
 			tree.append(nodes);
 
 			// 从url加载数据
-			loadNodes(tree, options["url"]);
+			loadNodes(tree, options);
 
 			// 初始化tree结构
 			initTree(tree);
 
 			// 给folder绑定事件
-			bindEvent(tree, options["url"]);
+			bindEvent(tree, options);
 		});
 	};
 })(jQuery);
